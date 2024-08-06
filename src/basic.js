d42865d0edad6acdda0c79708b8f65b293cf11fe
hansabie <raihansyah042@gmail.com>
2024-08-06 13:56:42 +0700

    testing

---
+++ src/basic.js
@@ -0,0 +1,22 @@
+export function sumAndPlusTen(a, b) {
+  return a + b + 10;
+}
+
+// export function maxNum(a, b) {
+//   if (typeof a !== "number" || typeof b !== "number")
+//     throw new Error("All of the arguments is a number!");
+//   if (b > a) {
+//     return b;
+//   } else if (a > b) {
+//     return a;
+//   } else {
+//     return a;
+//   }
+// }
+
+export function factorial(n) {
+  if (n < 0) throw new Error("Number must be positive integer");
+  if (n === 0 || n === 1) return 1;
+
+  return n * factorial(n - 1);
+}
---
+++ src/basic.test.js
@@ -0,0 +1,63 @@
+import { sumAndPlusTen, maxNum, factorial } from "./basic";
+import { describe, it, expect } from "vitest";
+
+// describe("sumAndPlusTen", () => {
+//   it("should return the sum of a+b+10 when a,b is integer", () => {
+//     //AAA pattern
+//     /// Arange - set up the test
+//     const a = 5;
+//     const b = 10;
+//     // //Act-execute the test
+//     const result = sumAndPlusTen(a, b);
+//     // //Assert - check the result
+//     expect(result).toBe(25);
+//   });
+
+//   it("should return NaN if any parameters is not provided", () => {
+//     const a = 5;
+//     const b = 10;
+
+//     const result = sumAndPlusTen();
+//     expect(result).toBeNaN();
+//   });
+// });
+
+// describe("maxNum", () => {
+//   it("should return the second argument when it is greater", () => {
+//     expect(maxNum(5, 10)).toBe(10);
+//   });
+
+//   it("should return the first argument when it is greater", () => {
+//     expect(maxNum(50, 10)).toBe(50);
+//   });
+
+//   it("should return the first argument when both arguments are equal ", () => {
+//     expect(maxNum(15, 15)).toBe(15);
+//   });
+
+//   it("should throw and error if one of the arguments is not a number", () => {
+//     expect(() => maxNum(5, "10")).toThrowError(
+//       "All of the arguments is a number!"
+//     );
+//   });
+// });
+
+describe("factorial", () => {
+  it("should return 1 when the input is 0 ", () => {
+    expect(factorial(0)).toBe(1);
+  });
+
+  it("should return 1 when the input is 1  ", () => {
+    expect(factorial(1)).toBe(1);
+  });
+
+  it(" should throw an error if one the arguments is negative ", () => {
+    expect(() => factorial(-10)).toThrowError(
+      "Number must be positive integer"
+    );
+  });
+
+  it("should return 3628800 when the input is 10  ", () => {
+    expect(factorial(10)).toBe(3_628_800);
+  });
+});
